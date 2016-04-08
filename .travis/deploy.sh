fe4894e4c1e5935cc6bc5aab363bb2b9b44c3c2e
iotonhcp <iot-on-hcp@mailinator.com>
2016-04-08 17:46:40 +0200

    adding deployment and restart script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,3 +1,22 @@
 #!/bin/bash 
+BRANCH=$1
+NEO=./target/sdk/tools/neo.sh
 
-ls -lah ./target/
+echo Deploying branch ${BRANCH}
+$NEO deploy -a "$ACCOUNT" \
+    -b "$BRANCH" \
+    -h "$LANDSCAPE_HOST" \
+    -u "$DEPLOY_USERNAME" \
+    -p "$DEPLOY_PASSWORD" \
+    -s ./target/com.sap.iot.starterkit.ui.war
+    
+echo "Restarting app...";
+
+$NEO restart -a "$ACCOUNT" \
+    -b "$BRANCH" \
+    -h "$LANDSCAPE_HOST" \
+    -u "$DEPLOY_USERNAME" \
+    -p "$DEPLOY_PASSWORD" \
+    --synchronous \
+    || return 1
+echo "DEPLOYMENT FINISHED"
